68e41c96b957cb0068d434c175dfb985d9afa49c
linh1999 <linhct020328@gmail.gmail.com>
2025-12-16 23:17:08 +0700

    add image

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,9 @@
 const Login: React.FC = () => {
     return (
-        <></>
+        <div className="relative max-w-[600px] mx-auto w-full backdrop-blur-md bg-white/5 border border-white/10 rounded-3xl lg:p-10 md:p-5 p-3 shadow-2xl overflow-hidden
+                    before:absolute before:inset-0 before:rounded-3xl before:bg-gradient-to-br before:from-emerald-500/20 before:via-transparent before:to-cyan-500/20 before:blur-2xl before:-z-10">
+
+        </div>
     )
 }
 
